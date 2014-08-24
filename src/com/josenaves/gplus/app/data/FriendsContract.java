79404e2b7de1a11fb6267bb48070465ebb4d89c1
Jose Naves Moura Neto <josenaves@gmail.com>
2014-08-24 00:50:15 -0300

    Putting all data to the contentprovider

--- src/com/josenaves/gplus/app/data/FriendsContract.java
+++ src/com/josenaves/gplus/app/data/FriendsContract.java
@@ -13,7 +13,7 @@ public final class FriendsContract implements BaseColumns {
 	
 	// The "content authority" is the name to use for all content provider uri's
 	// It's similar a domain name and a website. 
-	public static final String CONTENT_AUTHORITY = "com.josenaves.gplus.friends";
+	public static final String CONTENT_AUTHORITY = "com.josenaves.gplus";
 	
 	// Now this is base for for all URIs which app will use
 	public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
@@ -21,16 +21,7 @@ public final class FriendsContract implements BaseColumns {
 	// PATH
 	public static final String PATH_FRIEND = "friend";
 	
-	// FRIEND (dir)
-	// content://com.josenaves.gplus.friends/friend
-	
-	// FRIEND_ID
-	// friend (item)
-	// content://com.josenaves.gplus.friends/friend/[id]
-	
-
-	private FriendsContract() {
-	}
+	private FriendsContract() {}
 
 	 /* Inner class that defines the table contents of the friends table */
 	public static final class FriendsEntry implements BaseColumns {
@@ -41,10 +32,17 @@ public final class FriendsContract implements BaseColumns {
 		public static final Uri CONTENT_URI = BASE_CONTENT_URI.buildUpon().appendEncodedPath(PATH_FRIEND).build();
 		
 		// special mime-types that indicate that URI return directory (list of item) or a single item
+		
+		// FRIEND (dir)
+		// content://com.josenaves.gplus/friend
 		public static final String CONTENT_DIR_TYPE = ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + PATH_FRIEND;
+		
+		// FRIEND_ID (item)
+		// content://com.josenaves.gplus/friend/[id]
 		public static final String CONTENT_ITEM_TYPE = ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + PATH_FRIEND;
 		
 		public static final String COLUMN_NAME_ID = _ID;
+		public static final String COLUMN_NAME_GID = "gplusId";
 		public static final String COLUMN_NAME_NAME = "name";
 		public static final String COLUMN_NAME_IMAGE = "imageUrl";
 
